259016628838df829be2a3eb08e16e7fc06d3ded
sergio.fdz.gar@gmail.com <sergio.fdz.gar@gmail.com@b95e2650-25a3-a263-afd6-f310165af780>
2012-12-23 19:00:55 +0000

    He creado la nueva clase Tramo con la linea y la duracion del tramo.
    No he terminado de construir bien el grafo pero está muy cerca, yo voy entendiendo como funciona la clase de netdatastructures.
    
    Poco a poco, hay tiempo :-)
    
    ¡¡¡Feliz Navidad!!!

---
+++ trunk/metro/src/Tramo.java
@@ -0,0 +1,88 @@
+
+public class Tramo {
+	private int duracion;
+	private int linea;
+	/**
+	 * @param linea
+	 * @param duracion
+	 */
+	public Tramo(int duracion, int linea) {
+		this.duracion = duracion;
+		this.linea = linea;
+	}
+	
+	public Tramo(String duracion, String linea) {
+		this.duracion = Integer.parseInt(duracion);
+		this.linea = Integer.parseInt(linea);
+		
+	}
+	
+	/**
+	 * @return the linea
+	 */
+	public int getLinea() {
+		return linea;
+	}
+	/**
+	 * @param linea the linea to set
+	 */
+	public void setLinea(int linea) {
+		this.linea = linea;
+	}
+	/**
+	 * @return the duracion
+	 */
+	public int getduracion() {
+		return duracion;
+	}
+	/**
+	 * @param duracion the duracion to set
+	 */
+	public void setduracion(int duracion) {
+		this.duracion = duracion;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + linea;
+		result = prime * result + duracion;
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Tramo)) {
+			return false;
+		}
+		Tramo other = (Tramo) obj;
+		if (linea != other.linea) {
+			return false;
+		}
+		if (duracion != other.duracion) {
+			return false;
+		}
+		return true;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Tramo [linea=" + linea + ", duracion=" + duracion + "]";
+	}
+	
+	
+	
+}
