01ad218bce9ede7afab2dec533083c73d49964a9
sergio.fdz.gar@gmail.com <sergio.fdz.gar@gmail.com@b95e2650-25a3-a263-afd6-f310165af780>
2013-01-10 11:02:11 +0000

    Solucionado el problema al hacer el sentido inverso al imprimir la ruta.

--- metro/src/Dijkstra.java
+++ metro/src/Dijkstra.java
@@ -1,5 +1,7 @@
 import java.util.Iterator;
 import java.util.PriorityQueue;
+import java.util.Stack;
+
 
 import net.datastructures.AdjacencyListGraph;
 import net.datastructures.Edge;
@@ -14,26 +16,6 @@ public class Dijkstra<V, E> {
         /* Cola auxiliar con prioridad */
         private PriorityQueue<ElementoDecorado<Estacion>> Q;
 
-        private Edge<Tramo> buscarArista(
-				Vertex<ElementoDecorado<Estacion>> fuente,
-				Vertex<ElementoDecorado<Estacion>> antecesor) {
-			
-			Edge<Tramo> tramo=null;
-						
-			for (Edge<Tramo> e : grafo.incidentEdges(fuente)) {
-                Vertex<ElementoDecorado<Estacion>> z = grafo.opposite(fuente, e);
-			
-                if(antecesor.element().equals(z.element())){
-                	tramo=e;
-                	break;
-                }
-                else{
-                	tramo=null;
-                }
-			}
-			
-			return tramo;
-		}
 
         private Vertex<ElementoDecorado<Estacion>> buscarVertice(
 				ElementoDecorado<Estacion> u_entry) {
@@ -57,29 +39,27 @@ public class Dijkstra<V, E> {
         public Graph<ElementoDecorado<Estacion>,Tramo> devolverCamino(Vertex<ElementoDecorado<Estacion>> destino){
 			
         	Graph<ElementoDecorado<Estacion>, Tramo> camino = new AdjacencyListGraph<ElementoDecorado<Estacion>, Tramo>();
-			Vertex<ElementoDecorado<Estacion>> fuente=destino;
-			//Insertamos el vertice en el nuevo grafo.
-			camino.insertVertex(fuente.element());
-			Vertex<ElementoDecorado<Estacion>> antecesor=null;
-			Edge<Tramo> arista=null;
+			Vertex<ElementoDecorado<Estacion>> aux=destino;
+			Vertex<ElementoDecorado<Estacion>> inicio=null;
+			Stack <Vertex<ElementoDecorado<Estacion>>> stack_estaciones= new Stack<Vertex<ElementoDecorado<Estacion>>>();
 			
-			//Mientras que no lleguemos al vertice de inicio
-			while(!fuente.element().equals(fuente.element().parent())){
-				//Buscamos su antecesor en el recorrido.
-				antecesor= buscarVertice(fuente.element().parent());
-				//Insertamos ese nuevo vertice en el grafo
-				camino.insertVertex(antecesor.element());
-				//Creamos el vértice que unirá los dos vertices.
-				//Buscamos la arista
-				arista= buscarArista(fuente, antecesor);
-				//Insertamos la arista
-				camino.insertEdge(fuente, antecesor,arista.element());
-				//Realizamos el cambio
-				fuente=antecesor;
+			//Volcamos a pila todas las estaciones
+			while(!aux.element().equals(aux.element().parent())){
+				stack_estaciones.push(aux);
+				aux= buscarVertice(aux.element().parent());
 			}
+			stack_estaciones.push(aux);
 			
-			//Dar la vuelta al grafo
-			
+			//Insertamos los datos en un nuevo grafo.
+			//Buscamos su antecesor en el recorrido.
+			inicio= stack_estaciones.pop();
+			camino.insertVertex(inicio.element());
+			while(!stack_estaciones.isEmpty()){
+				aux=stack_estaciones.pop();
+				camino.insertVertex(aux.element());
+				camino.insertEdge(inicio, aux, aux.element().aristaParent());
+				inicio=aux;
+			}
 			
 			return camino;
 		}
@@ -87,15 +67,18 @@ public class Dijkstra<V, E> {
 		public void dijkstraVisit(Vertex<ElementoDecorado<Estacion>>  v) {
         //Inicializamos los valores de los vertices para realizar el cálculo.
     	for (Vertex<ElementoDecorado<Estacion>> u : grafo.vertices()) {
-            int u_dist;
+            int u_dist=0;
 
             if (u == v) {
-                    u_dist = 0;
-                    u.element().setParent(v.element());
+                u_dist = 0;
+                u.element().setParent(v.element());
             } else {
-                    u_dist = INFINITO;
+                u.element().setParent(null);
+            	u_dist = INFINITO;
             }
             
+            u.element().setVisitado(false);
+            u.element().setAristaParent(null);
             u.element().setDistancia(u_dist);
             grafo.replace(u, u.element());
             Q.add(u.element());
@@ -116,7 +99,7 @@ public class Dijkstra<V, E> {
             for (Edge<Tramo> e : grafo.incidentEdges(inicio)) {
                 Vertex<ElementoDecorado<Estacion>> z = grafo.opposite(inicio, e);
 
-                if (!z.element().visitado()) {
+                if (!z.element().visitado() && e.element()!=null) {
                     int e_weight = e.element().getduracion();
                     int z_dist=z.element().distancia();
                     int tiempo=u_dist + e_weight;
@@ -127,7 +110,7 @@ public class Dijkstra<V, E> {
                     	//Indicamos cual es el nodo del que proviene
                     	z.element().setParent(inicio.element());
                     	//Guardamos la arista de la que proviene
-                    	//z.element().setAristaParent(buscarArista(fuente, antecesor))
+                    	z.element().setAristaParent(e.element());
                     	
                     	Vertex<ElementoDecorado<Estacion>> modificado= buscarVertice(z.element());
                     	
@@ -135,13 +118,13 @@ public class Dijkstra<V, E> {
                     	grafo.replace(modificado, z.element());
                     	                    	
                     }
-                	//Actualizamos la cola de los vertices.
-                	Q.clear();
-                	for (Vertex<ElementoDecorado<Estacion>> u : grafo.vertices()) {
-                        if (!u.element().visitado()) {
-                        	Q.add(u.element());
-                        }
-                    }
+                }
+            }
+           	//Actualizamos la cola de los vertices.
+        	Q.clear();
+        	for (Vertex<ElementoDecorado<Estacion>> u : grafo.vertices()) {
+                if (!u.element().visitado()) {
+                	Q.add(u.element());
                 }
             }
         }
@@ -154,7 +137,8 @@ public class Dijkstra<V, E> {
         }
 
 		public int getDist(Vertex<ElementoDecorado<Estacion>>  u) {
-                return (Integer) u.element().distancia();
+			Vertex<ElementoDecorado<Estacion>> aux=buscarVertice(u.element());
+            return aux.element().distancia();
         }
 }
 
